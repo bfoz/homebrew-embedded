8877e4817cba5d10c16a8c31994b91c3a3c20102
Brandon Fosdick <bfoz@bfoz.net>
2014-02-18 18:19:11 -0800

    Updated st-link for the 1.0.0 release
    Moved the Github URL to a head-block

--- stlink.rb
+++ stlink.rb
@@ -1,9 +1,14 @@
 require "formula"
 
 class Stlink < Formula
-  version '0'
+  version '1.0.0'
   homepage 'https://github.com/texane/stlink'
-  url 'https://github.com/texane/stlink.git'
+  url 'https://github.com/texane/stlink/archive/1.0.0.tar.gz'
+  sha1 'd55bbdd8c4c907be15b28d089fddc86e7a167766'
+
+  head do
+    url 'https://github.com/texane/stlink.git'
+  end
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
